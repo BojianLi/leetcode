fd51b8dc466427c71f758bca774377a25b6606fb
BojianLi <BojianLi8866@gmail.com>
2015-05-31 04:03:32 -0500

    Create 23.java

---
+++ 23.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int end = lists.length;
+        if(end == 0) {
+            return null;
+        }
+        end = end - 1;
+        while(end > 0) {
+            int begin = 0;
+            while(begin < end) {
+                lists[begin] = merge(lists[begin], lists[end]);
+                begin++;
+                end--;
+            }
+        }
+        return lists[0];
+    }
+    
+    private ListNode merge(ListNode n1, ListNode n2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        while(n1 != null || n2 != null) {
+            int a = (n1 == null) ? Integer.MAX_VALUE : n1.val;
+            int b = (n2 == null) ? Integer.MAX_VALUE : n2.val;
+            if(a < b) {
+                curr.next = new ListNode(a);
+                n1 = n1.next;
+            } else {
+                curr.next = new ListNode(b);
+                n2 = n2.next;
+            }
+            curr = curr.next;
+        }
+        return dummyHead.next;
+    }
+}
