6f752b628599a6253b8f2421582668b8ec50b764
BojianLi <BojianLi8866@gmail.com>
2015-06-03 14:32:07 -0500

    Create 12.java

---
+++ 12.java
@@ -0,0 +1,20 @@
+public class Solution {
+    private static final int[] values = {
+        1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+    };
+    private static final String[] symbols = {
+        "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+    };
+    public String intToRoman(int num) {
+        StringBuilder st = new StringBuilder();
+        for(int i = 0; i < values.length; i++) {
+            int k = num / values[i];
+            for(int j = 0; j < k; j++) {
+                st.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return st.toString();
+        
+    }
+}
