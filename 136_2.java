25f63cee026bd49b3a956f732007161df2217bdf
BojianLi <BojianLi8866@gmail.com>
2015-06-03 01:32:11 -0500

    Create 136_2.java

---
+++ 136_2.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int x : nums) {
+            if(set.contains(x)) {
+                set.remove(x);
+            } else {
+                set.add(x);
+            }
+        }
+        return set.iterator().next();
+    }
+}
