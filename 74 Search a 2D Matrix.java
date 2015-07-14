92bd3d92742d9857d0d0cef7a95dcd2e20fd632a
BojianLi <BojianLi8866@gmail.com>
2015-07-14 11:54:02 -0500

    Create 74 Search a 2D Matrix.java

---
+++ 74 Search a 2D Matrix.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int start = 0;
+        int end = m * n - 1;
+        while (start <= end) {
+            int mid = (end - start) / 2 + start;
+            int i = mid / n;
+            int j = mid % n;
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+}
