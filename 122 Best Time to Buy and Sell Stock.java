f6333d51fdb57aa54353ed2082be6aa529580cc7
BojianLi <BojianLi8866@gmail.com>
2015-07-19 20:20:59 -0500

    Create 122 Best Time to Buy and Sell Stock.java

---
+++ 122 Best Time to Buy and Sell Stock.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < prices[i - 1]) {
+                profit += prices[i - 1] - buy;
+                buy = prices[i];
+            }
+        }
+        profit += prices[prices.length - 1] - buy;
+        return profit;
+    }
+}
