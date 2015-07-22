cbb7591b1411765cb26f22b6ee4ec746932967f0
BojianLi <BojianLi8866@gmail.com>
2015-07-22 17:06:07 -0500

    Create 189 Rotate Array.java

---
+++ 189 Rotate Array.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length);
+        reverse(nums, 0, k);
+        reverse(nums, k, nums.length);
+    }
+    private void reverse(int[] nums, int start, int end) {
+        end--;
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
