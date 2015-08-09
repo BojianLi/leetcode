d9a87ff71db76acfe449769d62cd3a4ef0390c95
BojianLi <BojianLi8866@gmail.com>
2015-08-09 01:23:04 -0500

    Create 97 Interleaving String.java

---
+++ 97 Interleaving String.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        int l1 = s1.length();
+        int l2 = s2.length();
+        boolean[][] dp = new boolean[l1 + 1][l2 + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= l1; i++) {
+            dp[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0];
+        }
+        for (int j = 1; j <= l2; j++) {
+            dp[0][j] = s2.charAt(j - 1) == s3.charAt(j - 1) && dp[0][j - 1];
+        }
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if (s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j]) {
+                    dp[i][j] = true;
+                }
+                if (s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[l1][l2];
+    }
+}
